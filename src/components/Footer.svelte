0dfa3a6792fae9182bbedd0affe6dd0fbae42129
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-04 01:57:25 +0300

    Footer added

---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer>Copyright 2022 Job Ninja</footer>
+
+<style>
+  footer {
+    color: #bbbbbb;
+    text-align: center;
+    padding: 20px;
+    max-width: 400px;
+    margin: 40px auto 0;
+    border-top: 1px solid #f2f2f2;
+  }
+</style>

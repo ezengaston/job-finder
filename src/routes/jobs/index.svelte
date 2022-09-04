869f3f6f2e190a2075ae96638abd38803f84c043
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-04 18:24:17 +0300

    Jobs list added

--- src/routes/jobs/index.svelte
+++ src/routes/jobs/index.svelte
@@ -1,3 +1,21 @@
+<script context="module">
+  export async function preload(page, session) {
+    const res = await this.fetch("/jobs.json");
+    const jobs = await res.json();
+
+    return { jobs };
+  }
+</script>
+
+<script>
+  export let jobs;
+</script>
+
 <h2>All Current Jobs</h2>
+<ul>
+  {#each jobs as job}
+    <li><a href="/">{job.title}</a></li>
+  {/each}
+</ul>
 
 <style></style>
